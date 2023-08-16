f34770f0c68b2b154469fb0c3585b1415b8e4d65
Jorge Vinicius <jorgeviniciuslucas@hotmail.com>
2023-08-16 17:35:31 -0300

    + cards

---
+++ src/components/Card/index.js
@@ -0,0 +1,39 @@
+import iconeFavoritar from "./favoritar.svg"
+import iconeDesfavoritar from "./desfavoritar.svg"
+
+export default function Card({ capa, titulo, duracao, generos}) {
+    return ( 
+        <>
+            <div className="max-w-[240px] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div className="relative">
+                    <div>
+                        <img className="rounded-t-lg" src={capa} alt={titulo} />
+                    </div>
+                    <div className="absolute top-0 right-0 w-full">
+                        <div className="flex items-center justify-end">
+                            <img className="w-10 h-10 mr-4" src="https://observatoriodatv.uol.com.br/wp-content/uploads/2017/06/17796568_1412934622100308_3127437474611447633_n.png" alt="serviço de streaming" />
+                        </div>
+                    </div>
+                    <div className="absolute bottom-0 left-0 w-full bg-gradient-to-t from-gray-900">
+                        <div className="m-4 flex items-center justify-between">
+                            <img className="w-9 h-9" src="https://logodownload.org/wp-content/uploads/2017/07/classificacao-18-anos-logo-1.png" alt="classificação indicativa" />
+                            <img className="w-9 h-9" src={iconeFavoritar} alt="Favoritar" />
+                        </div>
+                    </div>
+                </div>
+                <div className="p-5">
+                    <div>
+                        <h5 className="mb-2 text-md font-bold text-gray-900 dark:text-white">{titulo}</h5>
+                    </div>
+                    <div className="w-full flex flex-wrap justify-between text-sm">
+                        <div className="text-gray-900">{duracao}</div>
+                        <ul className="flex items-center justify-start text-gray-900 font-sm">
+                            <li>{generos}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </>
+     );
+}
+
--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,7 +1,9 @@
-import Rodape from '../../components/Rodape'
-import Cabecalho from '../../components/Cabecalho'
-import Banner from '../../components/Banner'
+import Rodape from 'components/Rodape'
+import Cabecalho from 'components/Cabecalho'
+import Banner from 'components/Banner'
 import Titulo from 'components/Titulo'
+import Card from 'components/Card'
+import videos from 'json/bd.json'
 
 export const Inicio = () => {
   return (
@@ -9,6 +11,11 @@ export const Inicio = () => {
         <Cabecalho />
         <Banner imagem="home"/>
         <Titulo>Um lugar para guardar suas séries e filmes!</Titulo>
+        <section className="max-w-full md:max-w-[50%] mx-auto grid grid-cols-2 md:grid-cols-3 gap-4 pt-2">
+          {videos.map((video) => {
+            return <Card {...video} key={video.id} />
+          })}
+        </section>
         <Rodape>Desenvolvido por Alura</Rodape>
     </>
   )
