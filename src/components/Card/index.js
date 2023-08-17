8fe800dfdd1b7bc7df3eb1c9541c0711f2f88c7d
Jorge Vinicius <jorgeviniciuslucas@hotmail.com>
2023-08-17 17:41:46 -0300

    + favoritar

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,39 +1,47 @@
 import iconeFavoritar from "./favoritar.svg"
 import iconeDesfavoritar from "./desfavoritar.svg"
+import { useFavoritoContext } from "contexts/Favoritos";
 
-export default function Card({ capa, titulo, duracao, generos}) {
+export default function Card({ id, capa, titulo, duracao, generos}) {
+    const { favorito, adicionarFavorito } = useFavoritoContext();
+    const ehFavorito = favorito.some((fav) => fav.id === id);
+    const icone = !ehFavorito ? iconeFavoritar : iconeDesfavoritar;
     return ( 
-        <>
-            <div className="max-w-[280px] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <div className="relative">
-                    <div>
-                        <img className="rounded-t-lg" src={capa} alt={titulo} />
-                    </div>
-                    <div className="absolute top-0 right-0 w-full">
-                        <div className="flex items-center justify-end">
-                            <img className="w-10 h-10 mr-4" src="https://observatoriodatv.uol.com.br/wp-content/uploads/2017/06/17796568_1412934622100308_3127437474611447633_n.png" alt="serviço de streaming" />
-                        </div>
-                    </div>
-                    <div className="absolute bottom-0 left-0 w-full bg-gradient-to-t from-gray-900">
-                        <div className="m-4 flex items-center justify-between">
-                            <img className="w-9 h-9" src="https://logodownload.org/wp-content/uploads/2017/07/classificacao-18-anos-logo-1.png" alt="classificação indicativa" />
-                            <img className="w-9 h-9" src={iconeFavoritar} alt="Favoritar" />
-                        </div>
+        <div className="max-w-[280px] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <div className="relative">
+                <div>
+                    <img className="rounded-t-lg" src={capa} alt={titulo} />
+                </div>
+                <div className="absolute top-0 right-0 w-full">
+                    <div className="flex items-center justify-end">
+                        <img className="w-10 h-10 mr-4" src="https://play-lh.googleusercontent.com/yDjaHCaOn_O89vnY7eOKH6ElEBtJrmN2CSI4yhiP1_GVC2zrxXWSFGxO0lt9-CU0mV4" alt="serviço de streaming" />
                     </div>
                 </div>
-                <div className="p-5">
-                    <div>
-                        <h5 className="mb-2 text-md font-bold text-gray-900 dark:text-white">{titulo}</h5>
-                    </div>
-                    <div className="w-full flex flex-wrap justify-between text-sm">
-                        <div className="text-gray-900">{duracao}</div>
-                        <ul className="flex items-center justify-start text-gray-900 font-sm">
-                            <li>{generos}</li>
-                        </ul>
+                <div className="absolute bottom-0 left-0 w-full bg-gradient-to-t from-gray-900">
+                    <div className="m-4 flex items-center justify-end">
+                        {/* <img className="w-9 h-9" src="https://logodownload.org/wp-content/uploads/2017/07/classificacao-18-anos-logo-1.png" alt="classificação indicativa" /> */}
+                        <img 
+                            className="w-9 h-9" 
+                            src={icone} 
+                            alt="Favoritar" 
+                            onClick={() => {
+                                adicionarFavorito({ id, titulo, capa})
+                            }}/>
                     </div>
                 </div>
             </div>
-        </>
+            <div className="p-5">
+                <div>
+                    <h5 className="mb-2 text-md font-bold text-gray-900 dark:text-white">{titulo}</h5>
+                </div>
+                <div className="w-full flex flex-wrap justify-between text-sm">
+                    <div className="text-gray-900">{duracao}</div>
+                    <ul className="flex items-center justify-start text-gray-900 font-sm">
+                        <li>{generos}</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
      );
 }
 
--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,14 +1,19 @@
 import Banner from "components/Banner";
+import Card from "components/Card";
 import Container from "components/Container";
 import Titulo from "components/Titulo";
+import { useFavoritoContext } from "contexts/Favoritos";
 
 export default function Favoritos() {
+    const { favorito } = useFavoritoContext();
     return ( 
         <>
             <Banner imagem="favoritos"/>
             <Titulo>Meus filmes favoritos</Titulo>
             <Container>
-                
+                {favorito.map((fav) => {
+                    return <Card {...fav} key={fav.id} />
+                })}
             </Container>
         </>
     );
