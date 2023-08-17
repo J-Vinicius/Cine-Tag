7eddbea257063cad358b0f284fffe7e8527b21d8
Jorge Vinicius <jorgeviniciuslucas@hotmail.com>
2023-08-17 12:47:35 -0300

    + contexto - favorito

--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,18 @@ import Favoritos from "./pages/Favoritos";
 import { Inicio } from "./pages/Inicio";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Rodape from "components/Rodape";
+import FavoritosProvider from "contexts/Favoritos";
 
 export default function AppRoutes() {
     return(
         <BrowserRouter>
             <Cabecalho />
-                <Routes>
-                    <Route path="/" element={<Inicio />}></Route>
-                    <Route path="/favoritos" element={<Favoritos />}></Route>
-                </Routes>
+                <FavoritosProvider>    
+                    <Routes>
+                        <Route path="/" element={<Inicio />}></Route>
+                        <Route path="/favoritos" element={<Favoritos />}></Route>
+                    </Routes>
+                </FavoritosProvider>
             <Rodape>Desenvolvido por Alura</Rodape>
         </BrowserRouter>
     )
